6d3c1e143ee5fce3cccdca4e25f425ec713eb48f
Daniel Janík <01djanik@gmail.com>
2024-04-21 13:27:54 +0200

    Added Isotopes for first Elements

--- BP/Assets/_Scripts/Elements/Isotope.cs
+++ BP/Assets/_Scripts/Elements/Isotope.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DecayMode { Stable, Alpha_decay, Beta_decay, Proton_emission, Neutron_emission, Positron_emission, Electron_capture, Spontaneous_fission }
+
 [System.Serializable]
 [CreateAssetMenu(fileName = "New Isotope", menuName = "Elements/Isotope")]
-
 public class Isotope : ScriptableObject
 {
     [field: SerializeField] public Element BaseElement { get; private set; } // Relative abundance of the isotope
     [field: SerializeField] public int NeutronAmount { get; private set; } // Amount of neutrons
-    [field: SerializeField] public double RelativeAbundance { get; set; } // Relative abundance of the isotope
+    [field: SerializeField] public string Symbol { get; set; } // Symbol of the isotope
+    [field: SerializeField] public DecayMode DecayMode { get; set; } // Decay state of the isotope
 }
