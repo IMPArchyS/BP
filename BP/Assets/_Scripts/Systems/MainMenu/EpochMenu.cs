0dd5ff82b89fe77de9220dd7a7c4317d99c5fb2d
Daniel Janík <01djanik@gmail.com>
2024-04-24 12:59:21 +0200

    finished MainMenu prototype

--- BP/Assets/_Scripts/Systems/MainMenu/EpochMenu.cs
+++ BP/Assets/_Scripts/Systems/MainMenu/EpochMenu.cs
@@ -1,16 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EpochMenu : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+    [SerializeField] private Transform currentMenu;
+    [SerializeField] private Transform loadingMenu;
 
-    private void Update()
+
+    public async void StartSimulationScene(int sceneIndex)
     {
-        
+        currentMenu.gameObject.SetActive(false);
+        loadingMenu.gameObject.SetActive(true);
+        var scene = SceneManager.LoadSceneAsync(sceneIndex);
+        scene.allowSceneActivation = false;
+        do
+        {
+            await Task.Delay(100);
+        } while (scene.progress < 0.9f);
+        await Task.Delay(1000);
+        scene.allowSceneActivation = true;
+        loadingMenu.gameObject.SetActive(false);
     }
 }
