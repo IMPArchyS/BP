0a549f15fbc754a33263fbe33679d0c8a8d113ca
Daniel Janík <01djanik@gmail.com>
2024-05-03 17:59:50 +0200

    added Debug Text

--- BP/Assets/_Scripts/Util/DebugInfo.cs
+++ BP/Assets/_Scripts/Util/DebugInfo.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class DebugInfo : MonoBehaviour
 {
@@ -7,6 +8,11 @@ public class DebugInfo : MonoBehaviour
     public static DebugInfo Instance;
     public TextMeshProUGUI fpsText;
     public TextMeshProUGUI speedText;
+    public TextMeshProUGUI debugText;
+    public TextMeshProUGUI memText;
+    public TextMeshProUGUI renderText;
+    public TextMeshProUGUI activeObjectText;
+    public int maxDebugLines;
     private FPSMovement fpsController;
     #endregion
 
@@ -47,6 +53,7 @@ public class DebugInfo : MonoBehaviour
     {
         fpsText = GameObject.Find("FPSText").GetComponent<TextMeshProUGUI>();
         speedText = GameObject.Find("SPEEDText").GetComponent<TextMeshProUGUI>();
+        debugText = GameObject.Find("DebugLogText").GetComponent<TextMeshProUGUI>();
         fpsController = GameObject.Find("FPSCamera").GetComponent<FPSMovement>();
     }
     #endregion
@@ -62,5 +69,68 @@ public class DebugInfo : MonoBehaviour
         int fps = (int)(1f / Time.unscaledDeltaTime);
         fpsText.text = "FPS: " + Mathf.Round(fps);
     }
+
+    private void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        string color = "white";
+        switch (type)
+        {
+            case LogType.Warning:
+                color = "yellow";
+                break;
+            case LogType.Error:
+            case LogType.Exception:
+                color = "red";
+                break;
+        }
+
+        string newLog = $"<color={color}>{logString}\n{stackTrace}</color>\n";
+        string[] lines = (newLog + debugText.text).Split('\n');
+
+        // Limit to a certain number of lines
+        if (lines.Length >= maxDebugLines)
+        {
+            lines = lines.Take(maxDebugLines).ToArray();
+        }
+
+        debugText.text = string.Join("\n", lines);
+    }
+    private void ShowMemoryUsage()
+    {
+        long totalMemory = System.GC.GetTotalMemory(false);
+        memText.text = "";
+        memText.text = $"Memory: {totalMemory / (1024 * 1024)} MB\n" + memText.text;
+    }
+
+    private void ShowAmountOfRenderedObjects()
+    {
+        int renderedObjects = QualitySettings.pixelLightCount;
+        renderText.text = "";
+        renderText.text = $"Rendered Objects: {renderedObjects}";
+    }
+
+    private void ShowAmountOfActiveGameObjects()
+    {
+        int activeGameObjects = FindObjectsOfType<GameObject>().Count(go => go.activeInHierarchy);
+        activeObjectText.text = "";
+        activeObjectText.text = $"Active GameObjects: {activeGameObjects}";
+    }
     #endregion
+
+    private void Update()
+    {
+        ShowMemoryUsage();
+        ShowAmountOfRenderedObjects();
+        ShowAmountOfActiveGameObjects();
+    }
 }
